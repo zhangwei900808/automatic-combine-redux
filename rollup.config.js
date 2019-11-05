f4cd54b3ea5dae010aac8852d32ef53f4f6a46ea
张威 <zw900808@gmail.com>
2019-11-05 14:26:43 +0800

    压缩js,并发布0.0.3版本

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,9 @@
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
+import { terser } from "rollup-plugin-terser";
+import babel from "rollup-plugin-babel";
+import json from "rollup-plugin-json";
+
 // browser-friendly UMD build
 import pkg from "./package.json";
 export default [
@@ -12,7 +16,11 @@ export default [
     },
     plugins: [
       resolve(), // so Rollup can find `ms`
-      commonjs() // so Rollup can convert `ms` to an ES module
+      babel({
+        exclude: "node_modules/**"
+      }),
+      commonjs(), // so Rollup can convert `ms` to an ES module,
+      terser()
     ]
   },
 
@@ -25,6 +33,13 @@ export default [
   {
     input: "src/index.js",
     // external: ['ms'],
-    output: [{ file: pkg.main, format: "cjs" }, { file: pkg.module, format: "es" }]
+    output: [{ file: pkg.main, format: "cjs" }, { file: pkg.module, format: "es" }],
+    plugins: [
+      resolve(),
+      babel({
+        exclude: "node_modules/**"
+      }),
+      terser()
+    ]
   }
 ];
