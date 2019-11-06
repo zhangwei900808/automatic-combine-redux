8c6efb8fc32d5d1ca31b073d6067bb4c02bef37b
张威 <zw900808@gmail.com>
2019-11-06 14:29:03 +0800

    发布0.0.11版本

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import autoCombineSaga from "./autoCombineSaga";
 import autoCombineReducer from "./autoCombineReducer";
-
+export { autoCombineReducer, autoCombineSaga };
 export default { autoCombineReducer, autoCombineSaga };
